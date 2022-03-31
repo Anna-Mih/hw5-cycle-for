70fe89cdf2a792d7cf9452bcc14c745c90dbb0f3
anna-mih <anedbalskaya@yandex.ru>
2022-04-01 00:21:11 +0300

    modified SQRServiceTest.java

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
+    @ParameterizedTest
     @CsvSource(
             value = {
                     "'interval 200-300',200,300,3",
@@ -13,50 +14,13 @@ class SQRServiceTest {
                     "'interval -200-(-100)',-200,-100,0",
                     "'interval -200-200',-200,200,5",
                     "'interval 0-1_000_000',0,1000000,90",
-                    "'interval -100-0',-100,0,0"
+                    "'interval -100-0',-100,100,1"
             },
             delimiter = ','
     )
-    @ParameterizedTest
-    void shouldCalculateSQR200And300(String test, long limit1, long limit2, long expected) {
+    void shouldCalculateSQR(String test, long limit1, long limit2, long expected) {
         SQRService service = new SQRService();
         long actual = service.calculateSQR(limit1, limit2);
         assertEquals(expected, actual);
     }
-
-    @ParameterizedTest
-    void shouldCalculateSQR0And1(String test, long limit1, long limit2, long expected) {
-        SQRService service = new SQRService();
-        long actual = service.calculateSQR(limit1, limit2);
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    void shouldCalculateSQRNeg200AndNeg100(String test, long limit1, long limit2, long expected) {
-        SQRService service = new SQRService();
-        long actual = service.calculateSQR(limit1, limit2);
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    void shouldCalculateSQRNeg200And200(String test, long limit1, long limit2, long expected) {
-        SQRService service = new SQRService();
-        long actual = service.calculateSQR(limit1, limit2);
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    void shouldCalculateSQR0And1_000_000(String test, long limit1, long limit2, long expected) {
-        SQRService service = new SQRService();
-        long actual = service.calculateSQR(limit1, limit2);
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    void shouldCalculateSQRNeg100And0(String test, long limit1, long limit2, long expected) {
-        SQRService service = new SQRService();
-        long actual = service.calculateSQR(limit1, limit2);
-        assertEquals(expected, actual);
-    }
-
 }
